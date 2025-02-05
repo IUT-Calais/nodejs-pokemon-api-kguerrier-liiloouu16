c65e13461c2e5643e93074d1b68bd256ff2f9b08
liiloouu16 <li.chatelain@hotmail.fr>
2025-02-05 10:28:34 +0100

    pokemoncard_seed_schema_ok

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  //suppression de tous les posts (voir pages 29 du cours)
+  await prisma.pokemonCard.deleteMany();
   await prisma.type.deleteMany();
   await prisma.type.createMany({
     data: [
@@ -25,6 +27,39 @@ async function main() {
       { name: 'Steel' },
       { name: 'Fairy' },
     ],
+  });
+
+
+  const fireType = await prisma.type.findUnique({where: {name: "Fire"}});
+  const fairyType = await prisma.type.findUnique({where: {name: "Fairy"}});
+
+  if (!fireType || !fairyType) {
+    throw new Error("Erreur : un des types n'a pas été trouvé !");
+  }
+
+  await prisma.pokemonCard.create({
+    data:{ 
+      name: 'Flamiaou',
+      pokedexId: 725 ,
+      type: {connect: {id: fireType.id}} ,
+      lifePoints: 70 ,
+      size: 0.7 ,
+      weight: 4 ,
+      imageUrl: "https://www.pokepedia.fr/images/thumb/6/6c/Flamiaou-USUL.png/800px-Flamiaou-USUL.png" 
+    },
+
+  });
+
+  await prisma.pokemonCard.create({
+    data:{
+      name: 'Nymphali',
+      pokedexId: 700,
+      type: {connect: {id: fairyType.id}},
+      lifePoints: 90,
+      size: 0.8 ,
+      weight: 7 ,
+      imageUrl: "https://www.pokepedia.fr/images/thumb/8/83/Nymphali-XY.png/800px-Nymphali-XY.png" 
+    },
   });
 
   console.log('Seed completed!');
