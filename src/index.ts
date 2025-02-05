b4f9db7bab45595d9ef1824eb68b0bcf4c3c7c68
liiloouu16 <li.chatelain@hotmail.fr>
2025-02-05 10:45:49 +0100

    route_pokemoncard_ok

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
 import express from 'express';
+// import { userRoute } from './user/user.route';
+import { Request, Response } from 'express';
+import prisma from './client';
+
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
+//use c'est un middleware
 app.use(express.json());
 
+// app.use('/users', userRoute);
+
 export const server = app.listen(port);
+
+app.get('/pokemons-cards', async (_req: Request, _res: Response) => {
+    // Récupère tous les utilisateurs et leurs posts
+    const pokemons = await prisma.pokemonCard.findMany({ include: { type: true } });
+    _res.status(200).send(pokemons);
+});
+
+app.get('/pokemons-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Pokemon selon l\'id');
+});
+
+app.post('/pokemon-cards', (_req: Request, _res: Response) => {
+  _res.status(200).send('Enregistrer le pokemon');
+});
+
+app.patch('/pokemon-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Modification du pokemon');
+});
+
+app.delete('/pokemon-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Suppression du pokemon');
+});
+
 
 export function stopServer() {
   server.close();
