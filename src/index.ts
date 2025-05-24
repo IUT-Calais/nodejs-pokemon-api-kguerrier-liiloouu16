5ab9a9e6f9064b8f26dec1b17c89ff33eb7d5ca0
liiloouu16 <li.chatelain@hotmail.fr>
2025-05-24 22:52:36 +0200

    swagger accessible ! non terminé

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { pokemonRouter } from './pokemon/pokemon.router';
 import { userRouter } from './user/user.router';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +12,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/pokemons-cards', pokemonRouter);
 app.use('/users', userRouter);
+
+//chemin doc swagger
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/swagger.yaml'));
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export function stopServer() {
     server.close();
